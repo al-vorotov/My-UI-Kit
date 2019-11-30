834ba326714f9aaacc6affaf60f7b5329c156db7
al-vorotov <48530374+al-vorotov@users.noreply.github.com>
2019-12-01 01:37:59 +0300

    add Context (#2)

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import LayoutContext from './context'
+
+const LayoutProvider = ({children}) => {
+  const object = {
+
+  };
+
+  const [state, setState] = useState(object);
+
+  return (
+      <LayoutContext.Provider value={state}>
+        {children}
+      </LayoutContext.Provider>
+  );
+};
+
+export default LayoutProvider;
