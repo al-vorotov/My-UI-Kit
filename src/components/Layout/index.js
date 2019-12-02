0ef2faf66b83b2ac840947f50e0d063de7203c39
al-vorotov <al.vorotov@gmail.com>
2019-12-02 22:04:07 +0300

    add new component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+
+import LayoutNav from './components/LayoutNav'
+import getComponentByName from './helpers/GetComponentByName';
+import LayoutContext from './context';
+
+import style from './styles.module.scss'
+
 
 const Layout = () => {
+  const {name} = useContext(LayoutContext);
   return (
-      <div className="Layout">
-      </div>
+      <section className={style.dealsPage}>
+        <div className={style.dealsPage__header}>
+          <LayoutHeader />
+        </div>
+        <div className={style.dealsPage__content}>
+          <LayoutNav activeComponentName={name} />
+          <article>
+            {getComponentByName(name)}
+          </article>
+        </div>
+      </section>
   );
 };
 
